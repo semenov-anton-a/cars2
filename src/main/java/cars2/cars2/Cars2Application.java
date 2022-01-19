601619de61cf8664ba571e0aae50bd5fa1bf6484
Anton Semenov <semenov.anton.a@gmail.com>
2022-01-19 19:00:03 +0200

    Begin

---
+++ src/main/java/cars2/cars2/Cars2Application.java
@@ -0,0 +1,13 @@
+package cars2.cars2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cars2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cars2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cars2/cars2/Cars2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cars2.cars2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cars2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
