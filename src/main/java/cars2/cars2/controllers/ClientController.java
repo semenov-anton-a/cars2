09136d0ac4d45be57218277ad978ebe7e278b066
Anton Semenov <semenov.anton.a@gmail.com>
2022-01-20 03:55:32 +0200

    Relation Begin

--- src/main/java/cars2/cars2/controllers/ClientController.java
+++ src/main/java/cars2/cars2/controllers/ClientController.java
@@ -1,5 +1,6 @@
 package cars2.cars2.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import cars2.cars2.models.Car;
 import cars2.cars2.models.Client;
 import cars2.cars2.repo.CarRepository;
 import cars2.cars2.repo.ClientRepository;
@@ -27,6 +30,9 @@ public class ClientController
     public String getIndex( Model model )
     {
         model.addAttribute("title", "Add Client");
+        model.addAttribute("clients", clientRepository.findAll() );
+        
+        model.addAttribute("cars", carRepository.findAll() );
         System.out.println("HELLo");
         return "addclient";
     }
@@ -40,14 +46,25 @@ public class ClientController
         return "redirect:/addclient";
     }
 
-    // TODO::Relations
-    // @PostMapping(
-    //     path = "/addclient/relations",
-    //     consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE } ) 
-    // public String makeRelations( List<String> data ) 
-    // {
-    //     System.out.println( data );
-    //     // System.out.println(data);
-    //     return "redirect:/addclient";
-    // }
+    @PostMapping( path = "/addclient/relations" )
+    public String makeRelations( 
+        @RequestParam Long clientid,
+        @RequestParam (value = "carids[]") List<Integer> carids ) 
+    {
+        System.out.println("======RUN========");
+        
+        // ArrayList<Car> cars = (ArrayList<Car>) carRepository.findAllById( carids );
+        // System.out.println( cars.get(0) );
+        
+        // Iterable<Car> cars = carRepository.findAllById( carids );
+        
+
+
+
+
+        System.out.println("===============");
+        // System.out.println( clientid );
+        // System.out.println( carids );
+        return "redirect:/addclient";
+    }
 }
